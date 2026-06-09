fef00f2bd8512651d53f83e557e7180ed67d5bff
Gaurav Mandall <mandalgaurav550@gmail.com>
2026-06-10 03:33:53 +0530

    build: simplify next config for cloudflare pages build
    Co-authored-by: Gaurav <72154932+gauravmandall@users.noreply.github.com>

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  basePath: '/lens-hover',
+  output: 'export',
+  images: { unoptimized: true },
+};
 
 export default nextConfig;
